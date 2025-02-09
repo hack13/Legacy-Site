3706a8dac2b00173c3ac2d63de1665e0e5c8731b
Timothy Rogers <github@hack13.me>
2025-02-09 10:53:00 -0500

    Fixing RSS feed to be in proper order

--- src/pages/blog/rss.xml.js
+++ src/pages/blog/rss.xml.js
@@ -1,8 +1,10 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
+import dayjs from "dayjs";
 
 export async function GET(context) {
   const Blog = await getCollection("blog");
+  Blog.sort((a, b) => dayjs(b.data.published).diff(dayjs(a.data.published)));
   return rss({
     title: "Hack13 Ramblings",
     description: "A fox with a blog, that rambles about tech, games, and life.",
